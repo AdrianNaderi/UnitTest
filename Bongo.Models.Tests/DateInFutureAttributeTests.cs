82406f27332d119bd17cd45948d8fb2bac9a114a
Adrian Naderi <Adrian.Naderi@hotmail.se>
2022-09-07 19:21:33 +0200

    Add project files.

---
+++ Bongo.Models.Tests/DateInFutureAttributeTests.cs
@@ -0,0 +1,32 @@
+﻿using Bongo.Models.ModelValidations;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bongo.Models
+{
+    [TestFixture]
+    public class DateInFutureAttributeTests
+    {
+        [Test]
+        [TestCase(100, ExpectedResult = true)]
+        [TestCase(-100, ExpectedResult = false)]
+        [TestCase(0, ExpectedResult = false)]
+        public bool DateValidator_InptExpectedDateRange_DateValidity(int addTime)
+        {
+            DateInFutureAttribute dateInFutureAttribute = new (()=>DateTime.Now);
+            
+            return dateInFutureAttribute.IsValid(DateTime.Now.AddMinutes(addTime)); // -200 = false
+        }
+
+        [Test]
+        public void DateValidator_NotValidDate_ReturnErrorMessage()
+        {
+            var result = new DateInFutureAttribute();
+            Assert.AreEqual("Date must be in the future", result.ErrorMessage);
+        }
+    }
+}
---
+++ SparkyXUnit/ProductXUnitTests.cs
@@ -0,0 +1,40 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparky
+{
+    public class ProductXUnitTests
+    {
+        private Product _product;
+        public ProductXUnitTests()
+        {
+            _product = new Product();
+        }
+        [Fact]
+        public void GetProductPrice_PlatinumCustomer_ReturnPriceWith20Discount()
+        {
+           _product.Price = 50;
+
+            var result = _product.GetPrice(new Customer() { IsPlatinum = true });
+
+            Assert.Equal(40, result);
+        }
+
+        [Fact]
+        public void GetProductPriceMOQAbuse_PlatinumCustomer_ReturnPriceWith20Discount()
+        {
+            var customer = new Mock<ICustomer>();
+            customer.Setup(u => u.IsPlatinum).Returns(true);
+
+            _product.Price = 50 ;
+
+            var result = _product.GetPrice(customer.Object);
+
+            Assert.Equal(40, result);
+        }
+    }
+}
